5ff32ba8d0a85906638a69144ddfce2875c4f9f3
soniaman000 <amanswarnkar2001@gmail.com>
2023-02-09 09:58:08 +0530

    Initial commit

---
+++ src/main/java/com/New_Inbound/New_Inbound/NewInboundApplication.java
@@ -0,0 +1,13 @@
+package com.New_Inbound.New_Inbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewInboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewInboundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/New_Inbound/New_Inbound/NewInboundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.New_Inbound.New_Inbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewInboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
